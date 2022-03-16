c2661201986a6dc6013526d355b5ffce588d720f
LiuFeng6 <1336007004@qq.com>
2022-03-17 00:36:55 +0800

    代码报401、403

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+'''
+这个代码是用来下载股票财报文件的
+现在遇到反爬问题，爬虫报401，403错误
+'''
+
 import requests
-from http import cookiejar
-from lxml import etree
 session = requests.session()
 
-def get_download(id):
 
+def get_download(id):
     Referer = "http://basic.10jqka.com.cn/" + id + "/finance.html"
     header = {
         "Connection": "keep-alive",
         "Host": "basic.10jqka.com.cn",
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64)\
         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36",
-        "Referer": Referer,
+        "Referer": Referer
     }
-    # http://basic.10jqka.com.cn/300164/finance.html
+    # http://basic.10jqka.com.cn/300033/finance.html
+    # 上面的链接是Referer，下面一个是目标链接的例子
     # http://basic.10jqka.com.cn/api/stock/export.php?export=benefit&type=year&code=300033
     url = "http://basic.10jqka.com.cn/api/stock/export.php"
     param = {
@@ -25,6 +29,7 @@ def get_download(id):
     response = session.get(url=url, params=param, headers=header)
 
     if response == 200:
+        # 如果爬虫结果正常，将保存文件
         file_name = param['code'] + "_" + param['export'] + "_" + param['type'] + ".xls"
         with open(file_name, "wb") as f:
             f.write(response.content)
